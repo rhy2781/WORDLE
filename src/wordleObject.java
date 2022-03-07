591f263e9e308c5dce001b501478424f18447140
Robert Yamasaki <rhy2781@rit.edu>
2022-03-07 16:14:55 -0500

    added in more comments, going to shift design to reduce the amount of responsibilities in this class

--- src/wordleObject.java
+++ src/wordleObject.java
@@ -19,10 +19,12 @@ public class wordleObject {
         }
 
     }
+
     public wordleObject() throws FileNotFoundException{
         this.tracker = initializeCharacterTracker();
         this.wordbank = wordleList();
     }
+
     /**
      *  This method initializes our tracker, which we use to determine if a letter is in a word or not
      */
@@ -36,6 +38,7 @@ public class wordleObject {
         tracker.put('.', true);
         return tracker;
     }
+
     /**
      *  This method iterates through the text file database and initializes an array to contain all the entries
      */
@@ -50,23 +53,25 @@ public class wordleObject {
         }
         return wordbank;
     }
+
     /**
      *  This method determines if a guess could be a possible word, given the guess, tracker, and potential word.
      */
     public boolean isSame(String s1, String guess, HashMap<Character, Boolean> tracker){
         for(int i = 0; i < s1.length(); i++){
-            if(!tracker.get(s1.charAt(i))){
+            if(!tracker.get(s1.charAt(i))){ // if we find a letter that is not included in the tracker
                 return false;
             }
-            else if(guess.charAt(i) == '.'){
+            else if(guess.charAt(i) == '.'){ // if our letter is a '.' then we continue=
                 continue;
             }
-            else if(guess.charAt(i) != s1.charAt(i)){
+            else if(guess.charAt(i) != s1.charAt(i)){ // if a letter found does not match the given string, return
                 return false;
             }
         }
         return true;
     }
+
     /**
      *  This method iterates through the wordle list and generates a list of potential answers based on the letters
      *  that have the correct placement and correct letters.
@@ -80,10 +85,18 @@ public class wordleObject {
         }
         return same;
     }
+
     /**
      *  If a letter is not included we can exclude it from the list of possbilities.
      */
     public void letterNotIncluded(Character c){
         this.tracker.put(c, false);
+    }
+
+    /**
+     *  If a letter is included, then we take note of it and account for it being a more likely answer
+     */
+    public void letterIncluded(Character c){
+        this.tracker.put(c, true);
     }
 }
