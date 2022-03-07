95b617c258fbe22c2889af44067c795506ea9b15
Robert Yamasaki <rhy2781@rit.edu>
2022-03-07 16:48:01 -0500

    added starter test code for database class

---
+++ test/databaseTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class databaseTest {
+    @Test
+    public void testInitialize() throws FileNotFoundException {
+        database a  = new database();
+        String[] wordbank = a.wordleList();
+        assertEquals(wordbank[0], "aback");
+        assertEquals(wordbank[wordbank.length - 1], "zonal");
+    }
+
+}
