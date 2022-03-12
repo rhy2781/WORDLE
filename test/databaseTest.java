879c5ff05ad05f7b2e5502e388061e300105874b
Robert Yamasaki <rhy2781@rit.edu>
2022-03-11 20:14:59 -0500

    initial react thingz

--- test/databaseTest.java
+++ test/databaseTest.java
@@ -1,17 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class databaseTest {
+
+    private String[] wordbank;
+
+    @BeforeEach
+    public void init() throws FileNotFoundException {
+        database a  = new database();
+        wordbank = a.wordleList();
+    }
+
     @Test
     public void testInitialize() throws FileNotFoundException {
-        database a  = new database();
-        String[] wordbank = a.wordleList();
-        assertEquals(wordbank[1319], "nylon");
-        assertEquals(wordbank[0], "aback");
-        assertEquals(wordbank[wordbank.length - 1], "zonal");
+        assertNotNull(wordbank, "Wordbank is not initialized properly");
+    }
+
+    @Test
+    public void testEntries(){
+        assertEquals(wordbank[0], "aback", "First entry is not correct");
+        assertEquals(wordbank[1319], "nylon", "Entry in the middle is not correct");
+        assertEquals(wordbank[wordbank.length - 1], "zonal", " Last Entry is not correct");
     }
 
 }
